2c5979e9b476b2e53b68213766a81b0e78101fe9
artesarh <rbhtn@icloud.com>
2026-02-18 07:48:25 +0000

    c

--- dbt/macros/ep_calculator.sql
+++ dbt/macros/ep_calculator.sql
@@ -1,5 +1,6 @@
 {% macro ep_curve_from_ylt(ylt_ref, loss_column, n_simulations, key_columns) %}
 
+
     {% set n = n_simulations | int %}
 
     {% if 'yearid' not in key_columns | lower %}
@@ -7,11 +8,13 @@
     {% endif %}
 
 
+
     with return_periods_to_select as (
         select
             [1000, 500, 250, 200, 150, 100, 50, 30, 20, 10, 5] as return_period
         )
 
+    -- annual losses summed up by key columns
     ,  annual_losses as (
         select
             {{ key_columns }},
@@ -20,42 +23,68 @@
         group by {{ key_columns }}
     ),
 
+
+    -- max loss is for oep
     , max_losses as (
         select
             {{ key_columns }},
-            sum({{ loss_column }}) as annual_loss
+            max({{ loss_column }}) as annual_loss
         from {{ ylt_ref }}
         group by {{ key_columns }}
     )
 
-    , ranked_annual_losses as (
+    -- Combine them up so subsequent calc happens once on one table only
+    , combined_grouped_losses as (
+
+        select
+            'AEP' as ep_type,
+            *
+        from annual_losses
+
+        union all
+
+        select
+            'OEP' as ep_type,
+        from max_losses
+    )
+
+    -- rank the losses based on key columns + the ep_type from above
+    , ranked_losses as (
         select
             *,
             row_number() over (
+                partition by {{ key_columns }} , ep_type
                 order by annual_loss desc
             ) as rank_num
         from annual_losses
     )
 
-    , ranked_max_losses as (
+    -- do a little RP conversion and filter for the ranks we're interested in
+    , ranked_losses_with_return_period as (
+
         select
-            *,
-            row_number() over (
-                order by annual_loss desc
-            ) as rank_num
-        from annual_losses
+            {{ n }} / rank_num::float as return_period
+            , *
+        from ranked_losses
+        inner join return_periods_to_select select on select.return_period =
+            ranked_losses.return_period
+
     )
 
+    -- Finally supplement it with the AAL
+    , aal_table as (
+        select
+            'AAL' as ep_type,
+             0 as return_period,
+             0 as rank_num,
+    -- TODO: We need to get rid of the yearid from key_columns to do
+    -- average annual loss which is just the sum of all losses / n_simulations
+    -- so we do not want to group by yearid
+             {{ key_columns }}
+)
 
-    select
-        *,
-        rank_num::float / {{ n }} as ep_probability,
-        case
-            when rank_num::float / {{ n }} > 0
-            then 1 / (rank_num::float / {{ n }})
-        end as return_period
-    from ranked_losses
-    where rank_num <= {{ n }}
-    order by annual_loss desc
+
+    select * from ranked_losses_with_return_period
+
 
 {% endmacro %}
