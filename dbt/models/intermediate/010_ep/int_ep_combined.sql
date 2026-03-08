5bd0c366d457a433e06de0d8ef8ef86a41e7e70b
artesarh <rbhtn@icloud.com>
2026-03-08 22:51:31 +0000

    feat: Added hive store for analysis list

--- dbt/models/intermediate/010_ep/int_ep_combined.sql
+++ dbt/models/intermediate/010_ep/int_ep_combined.sql
@@ -5,51 +5,69 @@
 ) }}
 
 /*
-    Incremental EP curve calculation for both vendors.
+    Incremental EP curve calculation for both vendors using ep_curve_from_ylt macro.
     Only processes new/changed data on incremental runs.
-
-    This query calculates EP curve for both vendors.
-    First split them out as they have different n_sims
-    calc ep curve for both, union the result together.
-    Then join back to dim distinct ylts which contains the aggregation keys.
 */
 
-with source_ylt as (
-    select 
-      pk,
-      aggregation_key,
-      source_vendor,
-      run_date,
-      filename,
-      analysis_id, 
-      model_code,
-      year_id,
-      event_id,
-      loss
+with ylt_data as (
+    select *
     from {{ ref('stg_cat_modelling_results__ylts') }}
-)
-
-, dim_distinct_ylts as (
-
-
-
-) 
-, verisk_ylt as (
-    select * from source_ylt where source_vendor = 'verisk'
-)
-
-, risklink_ylt as (
-    select * from source_ylt where source_vendor = 'risklink'
-)
-
-, verisk_ep as (
-    ep_curve_from_ylt(verisk_ylt, loss,
-      10000, aggregation_key
+    
+    {% if is_incremental() %}
+    where aggregation_key in (
+        select distinct aggregation_key 
+        from {{ ref('stg_cat_modelling_results__ylts') }}
+        where run_date > (
+            select coalesce(max(run_date), '1900-01-01'::date) 
+            from {{ this }}
+        )
     )
-  )
+    or aggregation_key not in (
+        select aggregation_key from {{ this }}
+    )
+    {% endif %}
+),
+
+-- Verisk EP calculation using macro (10K simulations)
+verisk_ep as (
+    {{ ep_curve_from_ylt('ylt_data', 'loss', 10000, 'aggregation_key') }}
+),
+
+-- RiskLink EP calculation using macro (100K simulations)
+risklink_ep as (
+    {{ ep_curve_from_ylt('ylt_data', 'loss', 100000, 'aggregation_key') }}
+),
+
+-- Combine both vendors' EP curves
+ep_curves as (
+    select aggregation_key, ep_type, return_period, rank_num, annual_loss from verisk_ep
+    union all
+    select aggregation_key, ep_type, return_period, rank_num, annual_loss from risklink_ep
+),
+
+-- Lookup dimension data (run_date, source_vendor, etc.)
+dim_lookup as (
+    select *
+    from {{ ref('int_setup__dim_distinct_ylts') }}
+    {% if is_incremental() %}
+    where aggregation_key in (select aggregation_key from ylt_data)
+    {% endif %}
+),
+
+-- Final result with dimension attributes
+wrap_up as (
+    select
+        d.aggregation_key,
+        d.run_date,
+        d.source_vendor,
+        d.source_file,
+        d.analysis_id,
+        ep.ep_type,
+        ep.return_period,
+        ep.rank_num,
+        ep.annual_loss
+    from ep_curves ep
+    join dim_lookup d using (aggregation_key)
 )
 
-, risklink_ep as (
-  ep_curve_from_ylt(risklink_ylt, loss, 100000, aggregation)
-)
-
+select * from wrap_up
--- dbt/models/raw/hive_storage__raw_analysis_lists.sql
+++ dbt/models/raw/hive_storage__raw_analysis_lists.sql
@@ -1,18 +1,23 @@
-{{ config(materialized='view', schema='raw') }}
-
-select
-     date
-    ,source
-    ,type
-    ,filename
-    ,analysis_id
-    ,modelled_lob
-    ,region_peril
-    ,analysis_modifications
-    ,is_official
-from read_parquet(
+{{config(MATERIALIZED ='view',
+schema = 'raw') }}SELECT
+  DATE,
+  source,
+  type,
+  filename,
+  analysis_id,
+  modelled_lob,
+  region_peril,
+  analysis_modifications,
+  is_official,
+  AAL,
+  OEP_200,
+  OEP_1000,
+  AEP_200,
+  AEP_1000
+FROM
+  read_parquet(
     '{{ var("cat_results_path") }}/date=*/source=*/type=analysis_list/*.parquet',
-    hive_partitioning = true,
-    union_by_name = true,
-    filename = true
-)
+    hive_partitioning = TRUE,
+    union_by_name = TRUE,
+    filename = TRUE
+  )
